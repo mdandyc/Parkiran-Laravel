f1fe26a7622461478bac91bfb9eb5b7d5a424356
mdandyc <dandyforze@gmail.com>
2017-02-12 11:29:26 +0700

    menambahkan layout dengan bootstrap

--- resources/views/kendaraan/edit.blade.php
+++ resources/views/kendaraan/edit.blade.php
@@ -1,15 +1,37 @@
+@extends('layout.app')
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h3>CRUD Parkir</h3>
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<form action="/parkiran/{{$kendaraan->id}}" method="post">
+					<input type="hidden" name="_method" value="put">
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+					<div class="form-group">
+							<input type="text" name="plat_nomer" class="form-control" placeholder="Plat Nomor" value="{{$kendaraan->plat_nomer}}">
+							{!! ($errors->has('plat_nomer')) ? $errors->first ('plat_nomer' , '<p class="help-block">:message</p>' ) : '' !!}
+						</div>
+						<div class="form-group">
+							<input type="text" name="jam_masuk" class="form-control" placeholder="Jam Masuk" value="{{$kendaraan->jam_masuk}}">
+							{!! ($errors->has('jam_masuk')) ? $errors->first('jam_masuk' , '<p class="help-block">:message</p>' ) : '' !!} 
+						</div>
+						<div class="form-group">
+							<input type="text" name="jam_keluar" class="form-control" placeholder="Jam Keluar" value="{{$kendaraan->jam_keluar}}">
+							{!! ($errors->has('jam_keluar')) ? $errors->first('jam_keluar' , '<p class="help-block">:message</p>' ) : '' !!} 
+						</div>
+						<div class="form-group">
+							<input type="submit" class="btn btn-primary" value="Simpan">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
 
 
-<h1>Create Blog Post</h1>
 
-<form action="/parkiran/{{$kendaraan->id}}" method="post">
-	<input type="text" name="plat_nomer" placeholder="Plat Nomor" value="{{$kendaraan->plat_nomer}}"><br>
-	{{ ($errors->has('plat_nomer')) ? $errors->first('plat_nomer') : '' }} <br>
-	<input type="text" name="jam_masuk" placeholder="Jam Masuk" value="{{$kendaraan->jam_masuk}}"><br>
-	{{ ($errors->has('jam_masuk')) ? $errors->first('jam_masuk') : '' }} <br>
-		<input type="text" name="jam_keluar" placeholder="Jam Keluar" value="{{$kendaraan->jam_keluar}}"><br>
-	{{ ($errors->has('jam_keluar')) ? $errors->first('jam_keluar') : '' }} <br>
-	<input type="hidden" name="_method" value="put">	
-	<input type="hidden" name="_token" value="{{ csrf_token() }}"><br>
-	<input type="submit" name="name" value="Submit">
-</form>
+
--- resources/views/kendaraan/index.blade.php
+++ resources/views/kendaraan/index.blade.php
@@ -1,15 +1,42 @@
-{{ Session::get('message') }}
-<a href="/parkiran/create"><h3>Create</h3></a>
-@foreach($kendaraans as $kendaraan)
-<a href="/parkiran/{{$kendaraan->id}}">
-	<p>{{ $kendaraan -> plat_nomer }}</p></a>
-	<p>{{ $kendaraan -> jam_masuk }}</p>
-	<p>{{ $kendaraan -> jam_keluar }}</p>
-	<a href="/parkiran/{{$kendaraan->id}}/edit">Edit</a>
-	<form action="/parkiran/{{$kendaraan->id}}" method="post">
-		<input type="hidden" name="_method" value="delete">
-		<input type="hidden" name="_token" value="{{ csrf_token() }}">
-		<input type="submit" name="name" value="Delete">
-	</form>
-	<hr>
-@endforeach
+@extends('layout.app')
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h3>CRUD Parkir</h3>
+			<div class="panel panel-default">
+				<div class="panel-body">
+					@if(Session::has('message'))
+					    <div class="alert alert-success">
+				           {{ Session::get('message') }}
+				        </div>
+					@endif
+					<a href="/parkiran/create" class="btn btn-info pull-right">Tambah Data</a><br><br>
+					<table class="table table-striped">
+						<tr>
+							<th>Plat Nomor</th>
+							<th>Jam Masuk</th>
+							<th>Jam Keluar</th>
+							<th>Action</th>
+						</tr>
+					@foreach($kendaraans as $kendaraan)
+						<tr>
+						<td>{{ $kendaraan -> plat_nomer }}</td></a>
+						<td>{{ $kendaraan -> jam_masuk }}</td>
+						<td>{{ $kendaraan -> jam_keluar }}</td>
+							<td>
+							<form method="POST" action="/parkiran/{{$kendaraan->id}}" accept-charset="UTF-8">
+									<input type="hidden" name="_method" value="delete">
+		                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+		                            <a href="/parkiran/{{$kendaraan->id}}/edit" class="btn btn-primary">Edit</a>
+		                        	<input type="submit" class="btn btn-danger" onclick="return confirm('Anda yakin akan menghapus data ?');" value="Delete">
+		                        </form>
+							</td>
+						@endforeach
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
