9325e49fbb06036753e8b0243b2f5bdb40fb22d7
mdandyc <dandyforze@gmail.com>
2017-02-13 16:35:10 +0700

    menambah tabel user di bagian database sql

--- parkiran.sql
+++ parkiran.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 12 Feb 2017 pada 05.31
+-- Generation Time: 13 Feb 2017 pada 10.33
 -- Versi Server: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -40,7 +40,7 @@ CREATE TABLE `kendaraan` (
 --
 
 INSERT INTO `kendaraan` (`id`, `plat_nomer`, `jam_masuk`, `jam_keluar`, `created_at`, `updated_at`) VALUES
-(2, 'D 54687 EVA', '09:26:19', '14:54:59', NULL, '2017-02-11 21:24:51'),
+(2, 'D 54687 EV', '09:26:19', '14:54:59', NULL, '2017-02-13 01:12:33'),
 (3, 'F454687GG', '11:39:32', '12:39:32', '2017-02-11 20:06:09', '2017-02-11 20:06:09'),
 (4, 'D 11244 EF', '04:11:21', '05:11:21', '2017-02-11 20:08:03', '2017-02-11 20:08:03'),
 (6, 'Z 4574 EF', '09:24:32', '10:24:32', '2017-02-11 21:27:46', '2017-02-11 21:27:46');
@@ -64,6 +64,45 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2017_02_12_023335_create_kendaraan_table', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `level` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'member',
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `level`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Muhammad Dandy C', 'dandyforze@gmail.com', '$2y$10$KU/8zXqUNvaJU6H/GwwTz.B/bBQaKXKfajcCmVtYNlnlhrXGPDjkC', 'member', 'z8KcRaXVmHQWHqrvNmHZjGdOoUHsPsOjmhdQ3NolfTgZX94Hw4tgrk9T4Les', '2017-02-13 00:39:11', '2017-02-13 00:39:11'),
+(2, 'Agan Toya', 'dandyforze@outlook.com', '$2y$10$srmvBzLWuy3gZLlo3fylx.BworBOsX3uRJUWXxQSQaAT4TKhvqnea', 'member', '1TKTuIeSF78ZLNHmQYZvs5PUVnPxbsCAY0Mh6XkZTmPZffgYfbHDhpDsglNs', '2017-02-13 01:23:31', '2017-02-13 01:23:31'),
+(3, 'Toyato', 'agantoya@gmail.com', '123123asd', 'admin', NULL, NULL, NULL),
+(4, 'Dandy Admin', 'admin@gmail.com', 'admin', 'member', NULL, NULL, NULL);
+
 --
 -- Indexes for dumped tables
 --
@@ -81,6 +120,12 @@ ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -94,6 +139,11 @@ ALTER TABLE `kendaraan`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
