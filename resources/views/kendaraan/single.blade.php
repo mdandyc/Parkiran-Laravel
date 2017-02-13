cf21a528eed6772a08e130fc1fee980435da81c4
mdandyc <dandyforze@gmail.com>
2017-02-13 16:38:31 +0700

    mengedit layout

--- resources/views/kendaraan/single.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Halaman Single</h1>
-
-{{$kendaraan ->plat_nomer}}
-<br>
-{{$kendaraan ->jam_masuk}}
-<br>
-{{$kendaraan ->jam_keluar}}
--- storage/framework/views/519aac22d4fe1dc8fffa8fef67c924d80f3659a0.php
+++ storage/framework/views/519aac22d4fe1dc8fffa8fef67c924d80f3659a0.php
@@ -38,4 +38,4 @@
 
 
 
-<?php echo $__env->make('layout.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
