af469c63110e4eb6c4ad0c2f99bdc7c88eda8ab2
mdandyc <dandyforze@gmail.com>
2017-02-15 17:28:11 +0700

    multi auth users dan admin

---
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+     protected $redirectTo = '/admin/home';
+public function username()
+{
+    return 'username';
+}
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest.admin', ['except' => 'logout']);
+    }
+    public function showLoginForm()
+    {
+        return view('admin.auth.login');
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+        protected function guard()
+        {
+            return Auth::guard('admins');
+        }
+}
